0f433a5915fb60bef187f19d4ed019d21be618af
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-06-04 13:05:46 +0330

    20240604

--- utilsstorage/boto3/actions/generate_temporary_url.py
+++ utilsstorage/boto3/actions/generate_temporary_url.py
@@ -7,7 +7,7 @@ def generate_temporary_url(
         bucket: str,
         server_address_for_client: str,
         endpoint_url: str,
-        expires_in: int = 3600,
+        expires_in: int = 3600,  # seconds
 ) -> str:
     temp_url = client.generate_presigned_url(
         ClientMethod='get_object',
